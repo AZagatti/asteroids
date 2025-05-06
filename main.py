077ff487c59256fa382bdc3e756e5079d9d999a2
azagatti <andre.zagatti1@gmail.com>
2025-05-05 21:35:34 -0300

    feat: create triangle character

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        tick_value = clock.tick(60)
+        dt = tick_value / 1000
 
 if __name__ == "__main__":
     main()
